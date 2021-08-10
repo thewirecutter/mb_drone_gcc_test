5e81a2c833d9a5e0e5573cac51efc84752943cce
Nhat Bui <minhnhat992@gmail.com>
2021-08-10 13:02:07 -0400

    commit

--- create_json.py
+++ create_json.py
@@ -3,5 +3,7 @@ import os
 
 
 drone_cred = os.environ.get("DRONE_GCC_SECRET_DEV")
+json_file = json.dumps(drone_cred)
 with open("./DRONE_GCC_SECRET_DEV.json","w",encoding='utf-8') as f:
-    json.dump(drone_cred,f)
+    f.write(json_file)
+    f.close()
