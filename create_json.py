edb40bc129e740adab25e17dc9af04c64b22710e
Nhat Bui <minhnhat992@gmail.com>
2021-08-10 13:19:39 -0400

    try to add variable from json

--- create_json.py
+++ create_json.py
@@ -1,8 +1,7 @@
 import json
 import os
 
-
-drone_cred = os.environ.get("DRONE_GCC_SECRET_DEV")
-json_file = json.dumps(drone_cred)
-with open("./DRONE_GCC_SECRET_DEV.json","w",encoding='utf-8') as f:
-    f.write(json.dumps(json.loads(drone_cred)))
+if __name__ == '__main__':
+    drone_cred = os.environ.get("DRONE_GCC_SECRET_DEV")
+    with open("./DRONE_GCC_SECRET_DEV.json","w",encoding='utf-8') as f:
+        f.write(json.dumps(json.loads(drone_cred)))
