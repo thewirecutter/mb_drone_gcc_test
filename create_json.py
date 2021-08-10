46483a8441e7908574508b024e2d13a7766a07ab
Nhat Bui <minhnhat992@gmail.com>
2021-08-09 22:39:25 -0400

    test install gcloud

--- create_json.py
+++ create_json.py
@@ -5,4 +5,4 @@ import os
 drone_cred = os.environ.get("DRONE_GCC_SECRET_DEV")
 print(drone_cred)
 with open("./DRONE_GCC_SECRET_DEV.json","w",encoding='utf-8') as f:
-    json.dump(drone_cred,f)
+    json.dump(drone_cred,f,ensure_ascii=True)
